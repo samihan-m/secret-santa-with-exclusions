2357e6b088a49a238ae2c1653083b9df32377be5
PikaGoku <ssmuppirala@gmail.com>
2023-12-06 14:32:49 -0700

    Some refactoring + added arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use std::collections::{HashSet, HashMap};
+use std::iter::zip;
 use std::hash::{Hash, Hasher};
 use std::rc::Rc;
-use rand::{thread_rng, Rng};
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Input file path
+    #[arg(short, long, default_value = "./input_data.csv")]
+    input_file_path: String,
+
+    /// Output directory path
+    #[arg(short, long, default_value = "./matchings")]
+    output_directory_path: String,
+
+    /// Verbose flag
+    #[arg(short = 'v', long, default_value = "false")]
+    do_be_verbose: bool,
+}
 
 #[derive(Debug)]
 struct Participant {
@@ -33,6 +52,17 @@ struct Configuration {
     participants: HashSet<Rc<Participant>>,
     cannot_send_to: HashMap<Rc<Participant>, HashSet<Rc<Participant>>>,
     cannot_receive_from: HashMap<Rc<Participant>, HashSet<Rc<Participant>>>,
+}
+
+impl Configuration {
+
+    fn ensure_valid_permutation(&self, permutation: &Permutation) -> Result<(), String> {
+        // Test the permutation to see if it is a valid permutation of the participants
+        // i.e. it is a derangement and it satisfies the exclusion constraints
+        permutation.ensure_is_derangement()?;
+        permutation.ensure_exclusions_satisfied(&self)?;
+        Ok(())
+    }
 }
 
 #[derive(Hash, PartialEq, Eq)]
@@ -72,46 +102,41 @@ impl Permutation {
 
         Ok(Permutation { assignments })
     }
-    
-    fn new(assignments: HashSet<Assignment>, participants: &HashSet<Rc<Participant>>) -> Permutation {
-        Permutation::try_new(assignments, participants).unwrap()
-    }
 
-    fn is_derangement(&self) -> bool {
+    fn ensure_is_derangement(&self) -> Result<(), String> {
         // Test the permutation to see if it is a derangement of the participants
         // A derangement is a permutation of elements in a set in which no element appears in it's original position
 
         // Make sure no sender has themselves as a recipient
-        self.assignments.iter().all(|assignment|
-            assignment.sender != assignment.recipient
-        )
+        for assignment in self.assignments.iter() {
+            if assignment.sender == assignment.recipient {
+                return Err(format!("Invalid permutation: {:?} is sending to themselves", assignment.sender.name));
+            }
+        }
+        
+        Ok(())
     }
 
-    fn satisfies_exclusion_constraints(&self, configuration: &Configuration) -> bool {
+    fn ensure_exclusions_satisfied(&self, configuration: &Configuration) -> Result<(), String> {
         // Test the permutation to see if it satisfies the exclusion constraints of the participants
 
         for assignment in self.assignments.iter() {
-            // Make sure nobody is sending a present to somebody they excluded (i.e. the recipient_id is not in the excluded_recipient_id of the participant with ID sender_id)
+            // Make sure nobody is sending a present to somebody they excluded (i.e. tt_id is not in the excluded_recipient_id of the participant with ID sender_id)
             if configuration.cannot_send_to[&assignment.recipient].contains(&assignment.sender) {
-                println!("Invalid configuration: {:?} cannot send to {:?}", assignment.sender.name, assignment.recipient.name);
-                return false;
+                return Err(format!("Invalid permutation: {:?} cannot send to {:?}", assignment.sender.name, assignment.recipient.name));
             }
             // Make sure nobody is getting a present from somebody they excluded (i.e. the sender_id is not in the excluded_sender_id of the participant with ID recipient_id)
             if configuration.cannot_receive_from[&assignment.sender].contains(&assignment.recipient) {
-                println!("Invalid configuration: {:?} cannot receive from {:?}", assignment.sender.name, assignment.recipient.name);
-                return false;
+                return Err(format!("Invalid permutation: {:?} cannot receive from {:?}", assignment.sender.name, assignment.recipient.name));
             }
         }
 
-        println!("Valid configuration.");
-
-        true
-
+        Ok(())
     }
 
 }
 
-fn load_configuration_from_csv(file_path: &str) -> Configuration {
+fn read_configuration_from_csv(file_path: &str) -> Configuration {
     // Read the CSV file at the given path and return the Configuration (participants and exclusion constraints)
 
     let mut participants_by_name: HashMap<String, Rc<Participant>> = HashMap::new();
@@ -196,7 +221,7 @@ fn load_configuration_from_csv(file_path: &str) -> Configuration {
 
 }
 
-fn generate_valid_permutation(configuration: Configuration) -> Permutation {
+fn generate_valid_permutation(configuration: Configuration, do_be_verbose: bool) -> Permutation {
     // Repeatedly try different derangements until we find one that satisfies the exclusion constraints
 
     // We have an n x n matrix (where n is the number of participants)
@@ -207,77 +232,55 @@ fn generate_valid_permutation(configuration: Configuration) -> Permutation {
 
     let mut rng = thread_rng();
 
-    let mut possible_senders = Vec::from_iter(configuration.participants.iter());
-    let mut possible_recipients = Vec::from_iter(configuration.participants.iter());
-
-    let mut assignments: HashSet<Assignment> = HashSet::new();
-
-    let valid_permutation: Permutation;
     let mut loop_count: u128 = 0;
-
     loop {
         loop_count += 1;
-        println!("Trying permutation #{}:", loop_count);
-        while possible_senders.len() > 0 {
-            let sender_index = rng.gen_range(0..possible_senders.len());
-            let sender = possible_senders.swap_remove(sender_index);
-            
-            let recipient_index = rng.gen_range(0..possible_recipients.len());
-            let recipient = possible_recipients.swap_remove(recipient_index);
-    
-            let assignment = Assignment {
-                sender: Rc::clone(sender),
-                recipient: Rc::clone(recipient),
-            };
-            
-            assignments.insert(assignment);
-        }
-    
-        let permutation_attempt = Permutation::try_new(assignments, &configuration.participants);
-        assignments = HashSet::new();
+        if do_be_verbose { println!("Trying permutation #{}:", loop_count) };
 
-        match permutation_attempt {
-            Ok(permutation) => {
-                let is_derangement = permutation.is_derangement();
-                let satisfies_exclusion_constraints = permutation.satisfies_exclusion_constraints(&configuration);
-
-                if is_derangement && satisfies_exclusion_constraints {
-                    valid_permutation = permutation;
-                    break;
-                }
-
-                // Try again
-                if !is_derangement {
-                    println!("Invalid permutation: not a derangement");
-                }
-                if !satisfies_exclusion_constraints {
-                    println!("Invalid permutation: does not satisfy exclusion constraints");
-                }
-                possible_senders = Vec::from_iter(configuration.participants.iter());
-                possible_recipients = Vec::from_iter(configuration.participants.iter());
-            },
-            Err(_) => {
-                // Try again
-                possible_senders = Vec::from_iter(configuration.participants.iter());
-                possible_recipients = Vec::from_iter(configuration.participants.iter());
+        let participants_randomized = {
+            let mut participants: Vec<&Rc<Participant>> = Vec::from_iter(configuration.participants.iter());
+            participants.shuffle(&mut rng);
+            participants
+        };
+        let possible_assignments = zip(configuration.participants.iter(), participants_randomized).map(
+            |(p1, p2)| Assignment {
+                sender: Rc::clone(p1),
+                recipient: Rc::clone(p2),
             }
-        }
-    }
+        ).collect();
 
-    valid_permutation
+        let possible_permutation = Permutation::try_new(possible_assignments, &configuration.participants);
+        let permutation = match possible_permutation {
+             Err(message) => {
+                 if do_be_verbose { println!("{}", message) }
+                 continue;
+             },
+             Ok(permutation) => {
+                 match configuration.ensure_valid_permutation(&permutation) {
+                     Err(message) => {
+                         if do_be_verbose { println!("{}", message) }
+                         continue;
+                     },
+                     Ok(_) => permutation,
+                 }
+             }
+         };
+
+         return permutation;
+    }
 }
 
 fn write_matching_files(permutation: Permutation, output_directory: &str) -> String {
 
     // Create matchings directory if necessary
     if let Err(_) = fs::create_dir(output_directory) {
-        println!("Error creating output directory {}, assuming it already exists.", output_directory);
+        println!("Failed to create output directory {}, assuming it already exists.", output_directory);
     }
 
     // Create subfolder with timestamp
     let output_directory = format!("{}/{}", output_directory, chrono::Local::now().format("%Y-%m-%d_%H-%M-%S"));
     if let Err(_) = fs::create_dir(output_directory.clone()) {
-        println!("Error creating output directory {}, assuming it already exists.", output_directory);
+        println!("Failed to create output directory {}, assuming it already exists.", output_directory);
     }
 
     for assignment in permutation.assignments.iter() {
@@ -285,19 +288,23 @@ fn write_matching_files(permutation: Permutation, output_directory: &str) -> Str
         let sender = &assignment.sender;
         let recipient = &assignment.recipient;
 
-        let information = format!("You are the Secret Santa for {}! ({})\n\nAddress:\n{}\n\nTheir interests are:\n{}", recipient.name, recipient.discord_handle, recipient.full_name_and_address, recipient.interests);
+        let padding_disclaimer = "SCROLL DOWN TO SEE WHO YOU GOT\nTHIS IS TO HIDE IT FROM THE DISCORD EMBED\n".to_string();
+        let vertical_padding = &"|\n".repeat(25);
+        let information = &format!("You are the Secret Santa for {}! ({})\n\nAddress:\n{}\n\nTheir interests are:\n{}", recipient.name, recipient.discord_handle, recipient.full_name_and_address, recipient.interests);
 
-        fs::write(format!("{}/{}.txt", output_directory, sender.name), information).unwrap();
+        fs::write(format!("{}/{}.txt", output_directory, sender.name), format!("{}", padding_disclaimer + vertical_padding + information)).unwrap();
     }
 
     return output_directory;
 }
 
 fn main() {
+    let arguments = Args::parse();
+
     let start_time = std::time::Instant::now();
 
     println!("Loading configuration...");
-    let configuration = load_configuration_from_csv("./input_data.csv");
+    let configuration = read_configuration_from_csv(&arguments.input_file_path);
 
     println!("Loaded participants:");
     for participant in configuration.participants.iter() {
@@ -305,10 +312,10 @@ fn main() {
     }
 
     println!("Generating valid permutation...");
-    let permutation = generate_valid_permutation(configuration);
+    let permutation = generate_valid_permutation(configuration, arguments.do_be_verbose);
 
     println!("Writing matching files...");
-    let output_directory = write_matching_files(permutation, "./matchings");
+    let output_directory = write_matching_files(permutation, &arguments.output_directory_path);
     println!("Done! Wrote matchings to {}.", output_directory);
 
     let duration = start_time.elapsed();
